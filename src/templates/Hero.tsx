ff03c3ac846f2d47842f606a90268bbcd400cbab
Khalid Almohannadi <91622233+kmhndi@users.noreply.github.com>
2024-07-03 08:38:46 +0300

    Update Hero.tsx

--- src/templates/Hero.tsx
+++ src/templates/Hero.tsx
@@ -1,6 +1,8 @@
 import Link from 'next/link';
 
 import { Background } from '../background/Background';
+import { Button } from '../button/Button';
+import { HeroOneButton } from '../hero/HeroOneButton';
 import { Section } from '../layout/Section';
 import { NavbarTwoColumns } from '../navigation/NavbarTwoColumns';
 import { Logo } from './Logo';
@@ -9,7 +11,11 @@ const Hero = () => (
   <Background color="bg-gray-100">
     <Section yPadding="py-6">
       <NavbarTwoColumns logo={<Logo xl />}>
-       
+        <li>
+          <Link href="https://github.com/ixartz/Next-JS-Landing-Page-Starter-Template">
+            GitHub
+          </Link>
+        </li>
         <li>
           <Link href="/">Sign in</Link>
         </li>
@@ -17,16 +23,20 @@ const Hero = () => (
     </Section>
 
     <Section yPadding="pt-20 pb-32">
-      
+      <HeroOneButton
         title={
           <>
-            {'Reform Qatar\n'}
-            <span className="text-primary-500">Bright Future</span>
+            {'The modern landing page for\n'}
+            <span className="text-primary-500">React developers</span>
           </>
         }
         description="The easiest way to build a React landing page in seconds."
-        
-      
+        button={
+          <Link href="instagram.com/brightfuture.qa">
+            <Button xl>Bright Future Instagram</Button>
+          </Link>
+        }
+      />
     </Section>
   </Background>
 );
